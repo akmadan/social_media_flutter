2931155554ae28802dc88e6156b4e66ec5595581
Akshit Madan <akmadan1717@gmail.com>
2022-03-26 22:37:15 +0530

    changed example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,6 @@
-import 'package:example/app.dart';
 import 'package:flutter/material.dart';
+import 'package:social_media_flutter/widgets/icons.dart';
+import 'package:social_media_flutter/widgets/text.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,5 +11,156 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(title: 'Flutter Demo', home: App());
+  }
+}
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Social Handles'),
+        backgroundColor: Colors.black,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                    child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SocialWidget(
+                      placeholderText: 'akshitmadan_',
+                      iconData: SocialIconsFlutter.instagram,
+                      iconColor: Colors.pink,
+                      link: 'https://www.instagram.com/akshitmadan_/',
+                      placeholderStyle:
+                          TextStyle(color: Colors.black, fontSize: 20),
+                    ),
+                    SocialWidget(
+                      placeholderText: 'akmadan',
+                      iconData: SocialIconsFlutter.github,
+                      link: '',
+                      iconColor: Colors.black,
+                      placeholderStyle:
+                          TextStyle(color: Colors.black, fontSize: 20),
+                    ),
+                    SocialWidget(
+                      placeholderText: 'AkshitMadan6',
+                      iconData: SocialIconsFlutter.twitter,
+                      iconColor: Colors.lightBlue,
+                      link: '',
+                      placeholderStyle:
+                          TextStyle(color: Colors.black, fontSize: 20),
+                    ),
+                    SocialWidget(
+                      placeholderText: 'AkshitMadan',
+                      iconData: SocialIconsFlutter.youtube,
+                      link: '',
+                      iconColor: Colors.red,
+                      placeholderStyle:
+                          TextStyle(color: Colors.black, fontSize: 20),
+                    ),
+                    SocialWidget(
+                      placeholderText: 'Akshit Madan',
+                      iconData: SocialIconsFlutter.linkedin_box,
+                      link: '',
+                      iconColor: Colors.blueGrey,
+                      placeholderStyle:
+                          TextStyle(color: Colors.black, fontSize: 20),
+                    ),
+                    SocialWidget(
+                      placeholderText: 'akmadan1717',
+                      iconData: SocialIconsFlutter.apple,
+                      link: '',
+                      iconColor: Colors.black,
+                      placeholderStyle:
+                          TextStyle(color: Colors.black, fontSize: 20),
+                    ),
+                    SocialWidget(
+                      placeholderText: 'Akshit Madan',
+                      iconData: SocialIconsFlutter.behance,
+                      link: '',
+                      iconColor: Colors.deepPurple,
+                      placeholderStyle:
+                          TextStyle(color: Colors.black, fontSize: 20),
+                    ),
+                    SocialWidget(
+                      placeholderText: 'Akshit Madan',
+                      iconData: SocialIconsFlutter.discord,
+                      link: '',
+                      iconColor: Colors.blueAccent,
+                      placeholderStyle:
+                          TextStyle(color: Colors.black, fontSize: 20),
+                    ),
+                    SocialWidget(
+                      placeholderText: 'Akshit Madan',
+                      iconData: SocialIconsFlutter.paypal,
+                      link: '',
+                      iconColor: Colors.blueGrey,
+                      placeholderStyle:
+                          TextStyle(color: Colors.black, fontSize: 20),
+                    ),
+                    SocialWidget(
+                      placeholderText: 'Akshit Madan',
+                      iconData: SocialIconsFlutter.twitch,
+                      link: '',
+                      iconColor: Colors.deepPurpleAccent,
+                      placeholderStyle:
+                          TextStyle(color: Colors.black, fontSize: 20),
+                    ),
+                    SocialWidget(
+                      placeholderText: 'Akshit Madan',
+                      iconData: SocialIconsFlutter.facebook_box,
+                      link: '',
+                      iconColor: Colors.blueGrey,
+                      placeholderStyle:
+                          TextStyle(color: Colors.black, fontSize: 20),
+                    ),
+                    SocialWidget(
+                      placeholderText: 'Akshit Madan',
+                      iconData: SocialIconsFlutter.spotify,
+                      link: '',
+                      iconColor: Colors.green,
+                      placeholderStyle:
+                          TextStyle(color: Colors.black, fontSize: 20),
+                    ),
+                  ],
+                )),
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SocialWidget(
+                  placeholderText: '/AkshitMadan',
+                  iconData: SocialIconsFlutter.youtube,
+                  link: '',
+                  iconSize: 20,
+                  placeholderStyle: TextStyle(fontSize: 14, color: Colors.grey),
+                ),
+                SocialWidget(
+                  placeholderText: '/Akshit Madan',
+                  iconData: SocialIconsFlutter.linkedin_box,
+                  link: '',
+                  iconSize: 20,
+                  placeholderStyle: TextStyle(fontSize: 14, color: Colors.grey),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
